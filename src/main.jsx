163d7d943d869b8ee8483e3fe95cb849594167c4
srijanid <srijanidas06@gmail.com>
2024-09-14 22:43:02 +0530

    navbar and header

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/**
+ * @copyright 2024 srijanid
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/**
+ * Components
+ */
+import App from './App.jsx';
+
+/**
+ * CSS link
+ */
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
