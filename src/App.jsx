6a0c3d40927f8e4c811bc0be471d05ed79b9eb84
srijanid <srijanidas06@gmail.com>
2024-09-26 15:52:51 +0530

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
+import Review from "./components/Review";
 
 const App = () => {
   return (
@@ -17,6 +20,9 @@ const App = () => {
       <main>
         <Hero />
         <About />
+        <Skill />
+        <Work />
+        <Review />
       </main>
     </>
   );
